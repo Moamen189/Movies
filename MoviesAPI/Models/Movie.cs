705bd56d931a4c6eb0988d7f684e3ff85e3a603d
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-02-24 11:42:47 +0200

    Update

--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -2,5 +2,24 @@
 {
     public class Movie
     {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string? Title { get; set; }
+
+        public int Year { get; set; }
+
+        public double Rate { get; set; }
+
+        [MaxLength(2500)]
+        public string? Storeline { get; set; }
+
+        public byte[] Poster { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+
     }
 }
