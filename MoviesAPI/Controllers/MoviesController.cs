a437a3e035920df19c0d2b34ccbb8530fa8d4b5e
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-02-24 13:34:36 +0200

    Update

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -7,5 +7,34 @@ namespace MoviesAPI.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        
+        public async Task<IActionResult> Create([FromForm] MoviesDto dto)
+        {
+            using var DataStream = new MemoryStream();
+
+            await dto.Poster.CopyToAsync(DataStream);
+            var Movie = new Movie() { 
+                Title = dto.Title , 
+                Year = dto.Year ,
+                Storeline = dto.Storeline ,
+                Rate= dto.Rate ,
+                Poster= DataStream.ToArray() ,
+                GenreId = dto.GenreId ,
+                    };
+
+            await context.Movies.AddAsync(Movie); 
+               
+            context.SaveChangesAsync();
+
+            return Ok(Movie);
+        }
     }
 }
