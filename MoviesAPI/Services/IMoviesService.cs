8f7d9f7197a3c0d9229b8cdc6251129b018561e7
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-02-25 17:07:03 +0200

    MovieServices

--- MoviesAPI/Services/IMoviesService.cs
+++ MoviesAPI/Services/IMoviesService.cs
@@ -1,6 +1,16 @@
 ﻿namespace MoviesAPI.Services
 {
-    public interface IMoviesServices
+    public interface IMoviesService
     {
+        Task<IEnumerable<Movie>> GetAll();
+        Task<Movie> Getbyid(int id);
+
+        Task<Movie> Add(Movie movie);
+
+        Movie Update(Movie movie);
+
+        Movie Delete(Movie movie);
+
+
     }
 }
--- MoviesAPI/Services/MoviesService.cs
+++ MoviesAPI/Services/MoviesService.cs
@@ -1,6 +1,45 @@
-﻿namespace MoviesAPI.Services
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Services
 {
-    public class MoviesService
+    public class MoviesService : IMoviesService
     {
+        private readonly ApplicationDbContext context;
+
+        private new List<string> _allowedExtenstions = new List<string> { ".jpg", ".png" };
+
+        private long _maxAllowedSize = 1048576;
+        public MoviesService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Movie> Add(Movie movie)
+        {
+            await context.Movies.AddAsync(Movie);
+
+            context.SaveChanges();
+
+            return movie;
+        }
+
+        public Movie Delete(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetAll()
+        {
+          return  await context.Movies.OrderByDescending(x => x.Rate).Include(g => g.Genre).ToListAsync();
+        }
+
+        public async Task<Movie> Getbyid(int id)
+        {
+            return await context.Movies.Include(g => g.Genre).SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Movie Update(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
