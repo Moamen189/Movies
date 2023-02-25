b3247557bc9696e3e9f261bc12f141cf517a222b
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-02-25 12:21:18 +0200

    Update

---
+++ MoviesAPI/Services/GenresService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Services
+{
+    public class GenresService : IGenresService
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public GenresService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Genre> Add(Genre Genre)
+        {
+            
+
+            await context.Genres.AddAsync(Genre);
+            context.SaveChanges();
+
+            return Genre;
+        }
+
+        public Task<Genre> Delete(Genre Genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Genre>> GetAll()
+        {
+            var genres = await context.Genres.OrderBy(g => g.Name).ToListAsync();
+
+            return genres;
+        }
+
+        public Task<Genre> Update(Genre Genre)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoviesAPI/Services/IGenresService.cs
@@ -0,0 +1,13 @@
+﻿namespace MoviesAPI.Services
+{
+    public interface IGenresService
+    {
+        Task<IEnumerable<Genre>> GetAll();
+
+        Task<Genre> Add(Genre Genre);
+
+        Task<Genre> Update(Genre Genre);
+
+        Task<Genre> Delete(Genre Genre);
+    }
+}
